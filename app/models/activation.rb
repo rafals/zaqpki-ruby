510fa1077cbc344eebfeb1da7decf8482c1f93aa
Rafał Sobota <ravsobota@gmail.com>
2009-02-23 14:35:34 +0100

    Activation::random_string

--- app/models/activation.rb
+++ app/models/activation.rb
@@ -1,2 +1,12 @@
 class Activation < ActiveRecord::Base
+  before_save :generate_token
+  def generate_token
+    token = random_string(10)
+  end
+  def random_string(len)
+    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    newpass = ""
+    1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
+    return newpass
+  end
 end
