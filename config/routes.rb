f5522b5e69b0ef86b9cd07619ba2787211fc5498
Jędrzej Wiewióra <jedzej@laptop.localdomain>
2009-02-23 15:43:47 +0100

    #	modified:   app/controllers/account_controller.rb #	modified:   commit #	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'intro', :action => 'index'
+  map.root :controller => 'intro', :action => 'index' 
   map.connect 'page/:id', :controller => 'account', :action => 'dashboard'
   map.connect 'login', :controller => 'account', :action => 'login'
   map.connect 'signup', :controller => 'account', :action => 'signup'
