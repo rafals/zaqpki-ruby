50d0223bec2dabf157c70dd32ce0c063886640e2
Jędrzej Wiewióra <jedzej@laptop.localdomain>
2009-02-23 15:31:14 +0100

    routes signup i login

--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,10 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'account', :action => 'dashboard'
+  map.root :controller => 'intro', :action => 'index'
   map.connect 'page/:id', :controller => 'account', :action => 'dashboard'
+  map.connect 'login', :controller => 'account', :action => 'login'
+  map.connect 'signup', :controller => 'account', :action => 'signup'
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
